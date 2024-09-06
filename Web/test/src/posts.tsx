8d2f55a1c8fc2461b3a3ee807380cc61876ceb5d
luisda25 <luisdaf04@outlook.com>
2024-09-05 22:32:49 -0600

    modified create test

--- Web/test/src/posts.tsx
+++ Web/test/src/posts.tsx
@@ -31,11 +31,11 @@ export const PostEdit = () => (
 );
 
 export const CreatePost = () => (
-    <Create>
-        <SimpleForm>
-            <ReferenceInput source = "userId" reference = "users"/>
-            <TextInput source = "title"/>
-            <TextInput source = "body" multiline rows = {5}/>
-        </SimpleForm>
-    </Create>
+        <Create>
+            <SimpleForm>
+                <ReferenceInput source = "userId" reference = "users"/>
+                <TextInput source = "title"/>
+                <TextInput source = "body" multiline rows = {5}/>
+            </SimpleForm>
+        </Create>
 );
--- Web/test/tests/create.test.tsx
+++ Web/test/tests/create.test.tsx
@@ -1,16 +1,3 @@
-/*import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'; 
-import { CreatePost } from '../src/posts'; 
-
-describe('Crear post', () => {
-    it('debe cargar la pantalla de crear post', () => {
-        const { debug } = render(<CreatePost />);
-        debug();
-        expect(screen.queryByText(/./)).toBeNull();
-    });
-});
-*/
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; 
