6ec2e65ce8309884dc68ad721dd4a37139db2d56
luisda25 <luisdaf04@outlook.com>
2024-09-05 22:36:47 -0600

    modified logout test

--- Web/test/tests/logout.test.ts
+++ Web/test/tests/logout.test.ts
@@ -1,12 +1,10 @@
 import { authProvider } from '../src/authProvider';
 
-describe('authProvider', () => {
-    describe('logout', () => {
-        it('Debe quitar el username de localStorage', async () => {
-            const removeItemSpy = jest.spyOn(localStorage, 'removeItem');
-            await authProvider.logout({});
-            expect(removeItemSpy).toHaveBeenCalledWith('username');
-            removeItemSpy.mockRestore();
-        });
+describe('logout', () => {
+    it('Debe quitar el username de localStorage', async () => {
+        const removeItemSpy = jest.spyOn(localStorage, 'removeItem');
+        await authProvider.logout({});
+        expect(removeItemSpy).toHaveBeenCalledWith('username');
+        removeItemSpy.mockRestore();
     });
 });
