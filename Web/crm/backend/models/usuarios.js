13036c256b27f884428337a8b570e8a85d5dbd16
luisda25 <luisdaf04@outlook.com>
2024-09-19 20:13:42 -0600

    fixed login bug, modified users schema

--- Web/crm/backend/models/usuarios.js
+++ Web/crm/backend/models/usuarios.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const usuarios = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['administrador', 'lector'], default: 'lector' }
+    role: { type: String, enum: ['administrador', 'lector'], default: 'lector' },
+    nombre: { type: String, required: true },
+    email: { type: String, required: true },
+    telefono: { type: Number, required: true }
+
 });
 
 module.exports = mongoose.model('usuarios', usuarios);
