e192dd42481720e1bfe36601947b0af6d1e27aca
luisda25 <luisdaf04@outlook.com>
2024-09-20 21:24:19 -0600

    modified dataProvider.ts

--- Web/crm/frontend/src/dataProvider.ts
+++ Web/crm/frontend/src/dataProvider.ts
@@ -1,6 +1,24 @@
-import jsonServerProvider from "ra-data-json-server";
-import { API_URL } from './config';
+import { fetchUtils, DataProvider } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+//import { API_URL } from './config';
 
- const dataProvider = jsonServerProvider(API_URL);
+interface Auth {
+    token: string;
+}
 
- export default dataProvider;
+const httpClient = (url: string, options: fetchUtils.Options = {}): Promise<any> => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    if (options.headers instanceof Headers) {
+        const auth: Auth | null = JSON.parse(localStorage.getItem('auth') || 'null');
+        if (auth && auth.token) {
+            options.headers.set('Authorization', `Bearer ${auth.token}`);
+        }
+    }
+    return fetchUtils.fetchJson(url, options);
+};
+
+const dataProvider: DataProvider = jsonServerProvider('https://localhost:5001/api', httpClient);
+
+export default dataProvider;
