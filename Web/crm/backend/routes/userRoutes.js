89edc7549f00d89c03f8d65bb326dcdb00465629
luisda25 <luisdaf04@outlook.com>
2024-09-20 21:22:51 -0600

    modified donaciones and usuarios routes

--- Web/crm/backend/routes/userRoutes.js
+++ Web/crm/backend/routes/userRoutes.js
@@ -5,10 +5,10 @@ const authenticateJWT = require('../middleware/auth');
 
 router.post('/register', userController.register); // Ruta para registrar usuario (se podría crear un rol de super-usaurio para administrarlo y solicitar validación de token)
 router.post('/login', userController.login); // Ruta para login de usuario
-router.get('/', authenticateJWT, userController.getAllUsers); // Obtener todos los posts
-router.get('/:id', authenticateJWT, userController.getOneUser); // Obtener un post por ID
-router.post('/', authenticateJWT, userController.createUser); // Crear un nuevo post
-router.put('/:id', authenticateJWT, userController.updateUser); // Actualizar un post por ID
-router.delete('/:id', authenticateJWT, userController.deleteUser);
+router.get('/', authenticateJWT, userController.getAllUsuarios); // Obtener todos los posts
+router.get('/:id', authenticateJWT, userController.getUsuarioById); // Obtener un post por ID
+router.post('/', authenticateJWT, userController.createUsuario); // Crear un nuevo post
+router.put('/:id', authenticateJWT, userController.updateUsuario); // Actualizar un post por ID
+router.delete('/:id', authenticateJWT, userController.deleteUsuario);
 
 module.exports = router;
