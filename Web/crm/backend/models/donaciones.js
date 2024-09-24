91c356ba0d7af40c20f55299714cde86ea4b1bbc
luisda25 <luisdaf04@outlook.com>
2024-09-24 17:52:39 -0600

    modified mongo schemas for usuarios and donaciones

--- Web/crm/backend/models/donaciones.js
+++ Web/crm/backend/models/donaciones.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const donaciones = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
     nombre: { type: String, required: true },
     email: { type: String, required: true },
     telefono: { type: Number, required: true },
     monto: { type: Number, required: true },
-    tipo: { type: String, required: true },
+    tipo: { type: String, enum: ['Tarjeta', 'Efectivo', 'Especie']},
 });
 
 module.exports = mongoose.model('donaciones', donaciones);
--- Web/crm/backend/models/usuarios.js
+++ Web/crm/backend/models/usuarios.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const usuarios = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['administrador', 'lector'], default: 'lector' },
     nombre: { type: String, required: true },
-    email: { type: String, required: true },
-    telefono: { type: Number, required: true }
-
+    email: { type: String, required: true, unique: true },
+    telefono: { type: Number, required: true, unique: true },
 });
 
 module.exports = mongoose.model('usuarios', usuarios);
