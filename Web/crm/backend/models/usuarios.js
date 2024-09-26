e7097e01bb7cecbc5d1bc06951aec6c0fcbc47d0
luisda25 <luisdaf04@outlook.com>
2024-09-26 17:00:26 -0600

    modified usuarios schema

--- Web/crm/backend/models/usuarios.js
+++ Web/crm/backend/models/usuarios.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const usuarios = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['administrador', 'lector'], default: 'lector' },
+    usuario: { type: String, required: true, unique: true },
+    contraseña: { type: String, required: true },
+    rol: { type: String, enum: ['administrador', 'lector'], default: 'lector' },
     nombre: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     telefono: { type: Number, required: true, unique: true }
