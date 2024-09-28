d7d30dcfac79575b8a75f6c11285326dda240a3b
luisda25 <luisdaf04@outlook.com>
2024-09-27 18:51:42 -0600

    added date to schema

--- Web/crm/backend/models/donaciones.js
+++ Web/crm/backend/models/donaciones.js
@@ -5,7 +5,8 @@ const donaciones = new mongoose.Schema({
     email: { type: String, required: true },
     phone: { type: Number, required: true },
     amount: { type: Number, required: true },
-    type: { type: String, enum: ['Tarjeta', 'Efectivo', 'Especie'], required: true}
+    date: { type: Date, default: Date.now },
+    type: { type: String, enum: ['Tarjeta', 'Efectivo', 'Especie'], required: true }
 });
 
 module.exports = mongoose.model('donaciones', donaciones);
