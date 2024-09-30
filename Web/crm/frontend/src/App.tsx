d404a88fabd1cc9e059270aa06864900b42b6643
Gwenvito <toootiz.killer@gmail.com>
2024-09-30 16:24:59 -0600

    login pagina principal css

--- Web/crm/frontend/src/App.tsx
+++ Web/crm/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import './Css/Main.css';  // Este es el CSS general para toda la app
 import { Admin, Resource } from "react-admin";
 import { Layout } from "./Layout";
 import dataProvider from "./dataProvider";
@@ -11,10 +12,15 @@ import { UserCreate } from "./Usuarios";
 import { i18nProvider } from "./i18nProvider";
 import LoginPage from "./LoginPage";
 
-
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider} authProvider={authProvider} i18nProvider={i18nProvider} loginPage={LoginPage}>
-    <Resource name = "donations" options={{ label: 'Donaciones' }} list = { DonationList } edit = { DonationEdit } create = { DonationCreate }/>
-    <Resource name = "users" options={{ label: 'Usuarios' }}list = { UserList } edit = { UserEdit } create = { UserCreate }/>
+  <Admin
+    layout={Layout}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    i18nProvider={i18nProvider}
+    loginPage={LoginPage}
+  >
+    <Resource name="donations" options={{ label: 'Donaciones' }} list={DonationList} edit={DonationEdit} create={DonationCreate} />
+    <Resource name="users" options={{ label: 'Usuarios' }} list={UserList} edit={UserEdit} create={UserCreate} />
   </Admin>
 );
