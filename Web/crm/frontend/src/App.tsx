9f7b0a362df92be97e10a14422f853d9de3317c9
luisda25 <luisdaf04@outlook.com>
2024-10-02 13:44:37 -0600

    fixed conflicts with main

--- Web/crm/frontend/src/App.tsx
+++ Web/crm/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Admin, Resource, defaultDarkTheme, defaultLightTheme } from "react-admin";
+import { Admin, Resource, radiantDarkTheme, radiantLightTheme } from "react-admin";
 import './Css/Main.css';  // Este es el CSS general para toda la app
 import { Layout } from "./Layout";
 import dataProvider from "./dataProvider";
@@ -15,19 +15,18 @@ import LoginPage from "./LoginPage";
 import MyBarChart from "./Barras";
 import MyPieChart from "./pie_chart";
 import MyDashboard from "./DashBoard";
-import { deepmerge } from "@mui/utils" 
+import UserIcon from "@mui/icons-material/Group";
+import PostIcon from "@mui/icons-material/Book";
 
-const lightTheme = defaultLightTheme;
-const darkTheme = deepmerge(defaultDarkTheme, {palette: {mode: 'dark'}});
 
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider} authProvider={authProvider} 
   i18nProvider={i18nProvider} loginPage={LoginPage} dashboard={MyDashboard} 
-  theme={lightTheme} darkTheme={darkTheme}>
+  theme={radiantLightTheme} darkTheme={radiantDarkTheme}>
     
-    <Resource name = "donations" options={{ label: 'Donaciones' }} list = { DonationList } edit = { DonationEdit } create = { DonationCreate }/>
-    <Resource name = "users" options={{ label: 'Usuarios' }}list = { UserList } edit = { UserEdit } create = { UserCreate }/>
+    <Resource name = "donations" options={{ label: 'Donaciones' }} list = { DonationList } edit = { DonationEdit } create = { DonationCreate } icon={ PostIcon }/>
+    <Resource name = "users" options={{ label: 'Usuarios' }}list = { UserList } edit = { UserEdit } create = { UserCreate } icon={ UserIcon }/>
     <Resource name = "barchart" options={{ label: 'Gráfico de Barras' }} list = { MyBarChart }/>
   </Admin>
   )
