32e014a69abab0a4615057dd570aaabee5b494e0
luisda25 <luisdaf04@outlook.com>
2024-10-04 15:51:15 -0600

    modified donaciones model to declare mexico city timezone

--- Web/crm/backend/models/donaciones.js
+++ Web/crm/backend/models/donaciones.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+moment = require('moment-timezone');
 
 const donaciones = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: Number, required: true },
     amount: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
+    date: { 
+        type: Date,
+        default: () => moment.tz(Date.now(), 'America/Mexico_City').toDate()
+    },
     type: { type: String, enum: ['Tarjeta', 'Efectivo', 'Especie'], required: true }
 });
 
