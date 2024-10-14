38c484f34d4e5275ddefcaacfcc737d6f6785618
RoSosaTec <a01027913@tec.mx>
2024-10-14 12:51:57 -0600

    agregados cambios a las gráficas del dashboard de administrador

--- Web/crm/frontend/src/Gráficas/Barras.tsx
+++ Web/crm/frontend/src/Gráficas/Barras.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDataProvider } from 'react-admin';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { CircularProgress } from '@mui/material'; // Opcional: para mostrar un loader mientras se cargan los datos.
+import { useTheme } from '@mui/material/styles';
 
  // Opcional: para mostrar un loader mientras se cargan los datos.
 
@@ -11,6 +12,7 @@ const MyBarChart = () => {
   const dataProvider = useDataProvider();  // Hook para acceder al dataProvider
   const [data, setData] = useState([]);    // Estado para almacenar los datos
   const [loading, setLoading] = useState(true); // Estado para manejar la carga
+  const theme = useTheme();
 
   useEffect(() => {
     // Llamada a la API para obtener la lista de 'Donaciones'
@@ -33,12 +35,14 @@ const MyBarChart = () => {
   if (loading) {
     return <CircularProgress />;
   }
+  const axisColor = theme.palette.mode === 'dark' ? 'white' : 'black';
+
 return (
   <ResponsiveContainer width="100%" height={600}>
     <BarChart data={data} layout='vertical'>
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis type="number" dataKey="amount"/>
-      <YAxis type="category"dataKey="name" width={150}/>
+      <XAxis type="number" dataKey="amount" stroke={axisColor}/>
+      <YAxis type="category"dataKey="name" width={150} stroke={axisColor}/>
       <Tooltip itemStyle={{backgroundColor: 'white', color: 'black'}}/>
       <Legend />
       <Bar dataKey="amount" fill="#00D7C9" label="Cantidad"/>
--- Web/crm/frontend/src/Gráficas/Date_chart.tsx
+++ Web/crm/frontend/src/Gráficas/Date_chart.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useDataProvider } from 'react-admin';
+import { useDataProvider} from 'react-admin';
+import { useTheme } from '@mui/material/styles';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 
@@ -7,6 +8,7 @@ const DateChart = () => {
     const dataProvider = useDataProvider();  // Hook para acceder al dataProvider
     const [data, setData] = useState([]);    // Estado para almacenar los datos
     const [loading, setLoading] = useState(true);
+    const theme = useTheme();
 
     dataProvider.getList('donations', { pagination: { page: 1, perPage: 100 }, sort: { field: 'amount', order: 'ASC' } })
         .then(({ data }) => {
@@ -29,15 +31,15 @@ const DateChart = () => {
         });
 
 
-
+  const axisColor = theme.palette.mode === 'dark' ? 'white' : 'black';
 
 
   return (
     <ResponsiveContainer width="100%" height={400}>
       <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" />
-        <YAxis />
+        <XAxis dataKey="month" stroke={axisColor}/>
+        <YAxis stroke={axisColor}/>
         <Tooltip itemStyle={{backgroundColor: 'white', color: 'black'}}/>
         <Legend />
         <Line type="monotone" dataKey="donations" label="Ingresos" stroke="#00D7C9" activeDot={{ r: 10 }} />
